c316778e3b291de4f66df9b9fbc9e62fbbf3e5a6
taek <yoyo2521@naver.com>
2022-09-14 16:05:12 +0900

    data.js 틀

---
+++ web/js/data.js
@@ -0,0 +1,58 @@
+const qnaList = [
+  {
+    q: "??",
+    a: [
+      {
+        answer: "a",
+        type: "?",
+      },
+      {
+        answer: "b",
+        type: "?",
+      },
+    ],
+  },
+];
+
+var infoArray = [
+  {
+    favorite: "",
+    dislike: "",
+    char: "",
+    name: "",
+    title: "",
+    subtitle: "",
+    explain: "",
+    img: "",
+  },
+  {
+    favorite: "",
+    dislike: "",
+    char: "",
+    name: "",
+    title: "",
+    subtitle: "",
+    explain: "",
+    img: "",
+  },
+  {
+    favorite: "",
+    dislike: "",
+    char: "",
+    name: "",
+    title: "",
+    subtitle: "",
+    explain: "",
+    img: "",
+  },
+  {
+    favorite: "",
+    dislike: "",
+    char: "",
+    name: "",
+    title: "",
+    ssubtitle: "",
+    explain: "",
+    img: "",
+  },
+];
