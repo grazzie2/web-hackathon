3b97d3545d0790466f30db5d908445d6ff1fba9c
박지현 <74723044+jihyun1971104@users.noreply.github.com>
2022-09-14 16:00:56 +0900

    img

--- web/js/data.js
+++ web/js/data.js
@@ -1,1 +1,59 @@
-ㄴㄴ;
+const qnaList = [
+    {
+        q: "??",
+        a: [
+            {
+                answer: "a",
+                type: "?"
+            },
+            {
+                answer: "b",
+                type: "?"
+            }
+        ]
+    }
+]
+
+var infoArray = 
+    [
+        {
+            favorite: "",
+            dislike: "",
+            char: "",
+            name: "",
+            title: "",
+            subtitle: "",
+            explain: "",
+            img: ""
+        },
+        {
+            favorite: "",
+            dislike: "",
+            char: "",
+            name: "",
+            title: "",
+            subtitle: "",
+            explain: "",
+            img: ""
+        },
+        {
+            favorite: "",
+            dislike: "",
+            char: "",
+            name: "",
+            title: "",
+            subtitle: "",
+            explain: "",
+            img: ""
+        },
+        {
+            favorite: "",
+            dislike: "",
+            char: "",
+            name: "",
+            title: "",
+            subtitle: "",
+            explain: "",
+            img: ""
+        }
+    ]
